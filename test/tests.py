b180bb8339b217a64fd1938707514c308551c939
Gelst13 <Gelst13@rambler.ru>
2023-02-16 14:26:48 +0300

    I hope from now on they will be ignored due to .gitignore

---
+++ test/tests.py
@@ -0,0 +1,23 @@
+from hstest import FlaskTest, dynamic_test
+
+from .base import (
+    test_correct_request,
+    test_bad_request
+)
+
+
+class ServerTest(FlaskTest):
+    source = 'app'
+
+    funcs = [
+        test_correct_request,
+        test_bad_request
+    ]
+
+    @dynamic_test(data=funcs)
+    def test_correct_request(self, func):
+        return func(self)
+
+
+if __name__ == '__main__':
+    ServerTest().run_tests()
